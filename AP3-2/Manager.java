fcc0d2b673c48121d045276234539eada201b4ee
Tom Wallis <tom@tomwallis.net>
2014-12-01 22:25:50 +0000

    Working except matching and multithreading

--- AP3-2/Manager.java
+++ AP3-2/Manager.java
@@ -8,13 +8,13 @@ class Manager {
 	ConcurrentLinkedQueue<String> workQueue = new ConcurrentLinkedQueue<String>();
 
 	public void process(String[] args) {
-		
-		
+
+
 		// Print an error message if we don't have a valid input.
 		if(args.length == 0) { 
 			System.err.println("Usage: 'java -classpath fileCrawler pattern [directory]'"); 	
-		
-		// We have valid input! Let's do our thing. 	
+
+			// We have valid input! Let's do our thing. 	
 		} else {
 
 			String noOfThreadsEnv = System.getenv("CRAWLER_THREADS");
@@ -24,7 +24,7 @@ class Manager {
 				int noOfThreads = 1; // Default single thread.
 			}
 
-			
+
 			// Find the pattern, and determine whether it's a word or a BASH pattern.
 			String pattern = args[0];
 			this.regex = Regex.cvtPattern(pattern);
@@ -32,7 +32,7 @@ class Manager {
 
 			// Populate each directory we were passed. 
 			for (int index = 1; index < args.length; index++) {
-			 	this.workQueue.add(args[index]);
+				this.workQueue.add(args[index]);
 				while(!this.workQueue.isEmpty()) {
 					processDirectory(workQueue.poll());
 				}
@@ -42,30 +42,29 @@ class Manager {
 	}
 
 
-   public void processDirectory( String name ) {
-    try {
-         File file = new File(name);	// create a File object
-	 if (file.isDirectory()) {	// a directory - could be symlink
-    String entries[] = file.list();
-	    if (entries != null) {	// not a symlink
-               System.out.println(name);// print out the name
-               for (String entry : entries ) {
-                	if (entry.compareTo(".") == 0)
-                 		continue;
-               		if (entry.compareTo("..") == 0)
-                 		continue;
-			   		this.workQueue.add(name + "/" + entry);
-            		processDirectory(name+"/"+entry);
+	public void processDirectory( String name ) {
+		try {
+			File file = new File(name);	// create a File object
+			if (file.isDirectory()) {	// a directory - could be symlink
+				String entries[] = file.list();
+				if (entries != null) {	// not a symlink
+					for (String entry : entries ) {
+						if (entry.compareTo(".") == 0)
+							continue;
+						if (entry.compareTo("..") == 0)
+							continue;
+						this.workQueue.add(name + "/" + entry);
+						//processDirectory(name+"/"+entry);
+					}
 				}
-        }
-    } else {   // We found a file, deal with it using the inputted regex.
+			} else {   // We found a file, deal with it using the inputted regex.
 
-		if (file.getName().matches(this.regex)) {System.out.println(file.getName());}
+				if (file.getName().matches(this.regex)) {System.out.println(file.getName());}
 
-	}
-    } catch (Exception e) {
-    	System.err.println("Error processing "+name+": "+e);
-    }
+			}
+		} catch (Exception e) {
+			System.err.println("Error processing "+name+": "+e);
+		}
 	}
 
 }
