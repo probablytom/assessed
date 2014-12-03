462408d94847484117a418582af86b2d1a2cd90c
Tom Wallis <tom@tomwallis.net>
2014-12-03 01:38:07 +0000

    Working matches

--- AP3-2/Manager.java
+++ AP3-2/Manager.java
@@ -6,44 +6,11 @@ import java.util.regex.*;
 class Manager {
 
 
-	Pattern regex;
-	Matcher matcher;
+	String regex;
 	ConcurrentLinkedQueue<String> workQueue = new ConcurrentLinkedQueue<String>();
 	ConcurrentLinkedQueue<String> fileQueue = new ConcurrentLinkedQueue<String>();
 
-	/*public void process(String[] args) {
-
-
-		// Print an error message if we don't have a valid input.
-		if(args.length == 0) { 
-			System.err.println("Usage: 'java -classpath fileCrawler pattern [directory]'"); 	
-
-			// We have valid input! Let's do our thing. 	
-		} else {
-
-			String noOfThreadsEnv = System.getenv("CRAWLER_THREADS");
-			if (noOfThreadsEnv != null) {
-				Integer noOfThreads = Integer.parseInt( noOfThreadsEnv );
-			} else {
-				int noOfThreads = 1; // Default single thread.
-			}
-
-
-			// Find the pattern, and determine whether it's a word or a BASH pattern.
-			String pattern = args[0];
-			this.regex = cvtPattern(pattern);
-
-			// Populate each directory we were passed. 
-			for (int index = 1; index < args.length; index++) {
-				this.workQueue.add(args[index]);
-				while(!this.workQueue.isEmpty()) {
-					processDirectory(workQueue.poll());
-				}
-			}
-
-		} 	
-	}*/
-
+	
 	
 	public void process(String[] args) {
 		
@@ -53,22 +20,32 @@ class Manager {
 		
 		} else { // On with the show.
 			
-			this.regex = Pattern.compile( cvtPattern(args[0]) );
+			this.regex = cvtPattern(args[0]);
 			
 			// Seperate out files and directories.
-			
 			if (args.length == 1) {
-				populateQueue(new File("."));
+				try {
+					populateQueue(new File("."));
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
 			}
-			
-			System.out.println(this.regex.pattern());
+			 
 			
 			for (int index = 1; index < args.length; index++) {
 				this.workQueue.add(args[index]);
 				while(!this.workQueue.isEmpty()) {
-					populateQueue( new File(this.workQueue.poll()) );
+					try {
+						populateQueue( new File(this.workQueue.poll()) );
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+						e.printStackTrace();
+					}
 				}
 			}
+			
+			System.out.println(this.regex);
 			
 			
 			// Process all of the files we have.
@@ -76,66 +53,39 @@ class Manager {
 			while (!this.fileQueue.isEmpty()) {
 				current = this.fileQueue.poll();
 //				Matcher matcher = this.regex.matches(current);
-				boolean matched = Pattern.matches(cvtPattern(args[0]), current);
+				boolean matched = Pattern.matches(this.regex, current);
 				if (matched) System.out.println(current);
 			}
 		}
 		
 		
 	}
-
-	/*
-	public void processDirectory( String name ) {
-		try {
-			File file = new File(name);	// create a File object
-			if (file.isDirectory()) {	// a directory - could be symlink
-				String entries[] = file.list();
-				if (entries != null) {	// not a symlink
-					for (String entry : entries ) {
-						if (entry.compareTo(".") == 0)
-							continue;
-						if (entry.compareTo("..") == 0)
-							continue;
-						this.workQueue.add(name + "/" + entry);
-						//processDirectory(name+"/"+entry);
-					}
-				}
-			} else {   // We found a file, deal with it using the inputted regex.
-
-				if (file.getName().matches(this.regex)) {System.out.println(file.getName());}
-
-			}
-		} catch (Exception e) {
-			System.err.println("Error processing "+name+": "+e);
-		}
-	}*/
 	
 	
 
 
 	/**
-	 * Note: Code copied from https://gist.github.com/yangls06/5464683.
-	 * If time permits, this will be written by Tom Wallis. 
-	 * For as long as this note persists, credit for this code is handed to the original authour.
+	 * Note: Code borrowed heavily from https://gist.github.com/yangls06/5464683. Modifications were then made to make this work as intended.
+	 * If time permits, this will be entirely rewritten by Tom Wallis, as opposed to partially. 
+	 * For as long as this note persists, some credit for this code is handed to the original authour.
 	 */
 	private String cvtPattern(String line) {
 		line = line.trim();
 		int strLen = line.length();
 		StringBuilder sb = new StringBuilder(strLen);
-		// Remove beginning and ending * globs because they're useless
-		if (line.startsWith("*") || line.startsWith("'"))
-		{
+		
+		if (line.charAt(0) == '\'') {	// double quoting on Windows
+			strLen--;
 			line = line.substring(1);
-			strLen--;
 		}
-		if (line.endsWith("*") || line.endsWith("'"))
-		{
-			line = line.substring(0, strLen-1);
+		if (line.endsWith("\'")) {
 			strLen--;
+			line = line.substring(0, line.length()-1);
 		}
+		
 		boolean escaping = false;
 		int inCurlies = 0;
-		sb.append('/');
+		sb.append("^");
 		for (char currentChar : line.toCharArray())
 		{
 			switch (currentChar)
@@ -215,26 +165,28 @@ class Manager {
 				sb.append(currentChar);
 			}
 		}
-		sb.append('/');
+		sb.append("$");
 		return sb.toString();
 	}
 	
-	public void populateQueue(File toPopulate) {
+	public void populateQueue(File toPopulate) throws Exception {
 		if (toPopulate.isDirectory()) {
 			String entries[] = toPopulate.list();
 			for (String entry : entries) {
 				// If the entry represents another directory, add it to the workQueue.
-				File fEntry = new File(entry);
+				File fEntry = new File(toPopulate.getAbsolutePath() + "/" + entry);
+				boolean fEntryDir = fEntry.isDirectory();
 				if (fEntry.isDirectory()) {
-					this.workQueue.add(entry);
+					this.workQueue.add(fEntry.getAbsolutePath());
 				} else {
 					this.fileQueue.add(entry);
 				}
 			}
 		} else {
-			System.out.println("Things are worse than you feared.");
+			throw new Exception(); // This should NEVER happen.
 		}
 	}
 
 
 }
+
