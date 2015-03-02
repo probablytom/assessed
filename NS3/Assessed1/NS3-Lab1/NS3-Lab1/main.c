8ed107d2e7b7d2ddd3511d045370e1e04941063e
Tom Wallis <tom@tom-wallis.com>
2015-03-02 19:12:41 +0000

    Set up xcode project for NS3...

---
+++ NS3/Assessed1/NS3-Lab1/NS3-Lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NS3-Lab1
+//
+//  Created by Tom Wallis on 02/03/2015.
+//  Copyright (c) 2015 Tom Wallis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
