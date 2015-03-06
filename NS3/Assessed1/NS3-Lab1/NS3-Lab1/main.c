107bb4a85212acdd527f9d935a5c46f729cfe0e3
Tom Wallis <tom@tom-wallis.com>
2015-03-06 15:10:28 +0000

    Messy, but now reading short hostname.

--- NS3/Assessed1/NS3-Lab1/NS3-Lab1/main.c
+++ NS3/Assessed1/NS3-Lab1/NS3-Lab1/main.c
@@ -8,38 +8,28 @@
 //  Of note: development was helped by the guide at http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
 //
 //
-//  TODO: Catch all buffer overflows.
 //
 //
 
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
 #include <netdb.h>
-#include <signal.h>
-#include <arpa/inet.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <strings.h>
 
 
 
 // Defining constants...
 #define BUFFERLEN 65536
 #define MAXPATHLENGTH 1024
+#define MAXHOSTLENGTH 1024
 #define PORT "8068"
 #define BACKLOG 10
 #define PACKETSIZE 512 // Packet size in bytes.
-
-// Getting horrible path problems, so saving the working directory here.
-const char *abs_path;
 
 
 int send_server_error(int connfd) {
@@ -62,20 +52,6 @@ void report_errno(char *message) {
 }
 
 
-long get_file_contents(char *filepath_in, char *file_contents) {  // not getting past this
-    FILE  *fd = fopen(filepath_in, "r");
-    long file_length = 0;
-
-    int char_read = getc(fd);
-    while( char_read != EOF ) {
-        *(file_contents+file_length) = char_read;
-        char_read = getc(fd);
-        file_length++;
-    }
-    return file_length;
-}
-
-
 int send_file_not_found(int connfd) {
     if(send(connfd, "HTTP/1.0 404 FILE NOT FOUND\r\n\r\n404 FILE NOT FOUND", 27,0 ) == -1) {
         // Error here too!
@@ -84,6 +60,36 @@ int send_file_not_found(int connfd) {
     return 0;
 }
 
+
+
+long get_file_contents(char *filepath, char *file_contents) {
+    // Check the file actually exists.
+    struct stat file_data;
+    if (stat(filepath, &file_data) == 0) {
+        FILE  *fd = fopen(filepath, "r");
+        long file_length = 0;
+
+        int char_read = getc(fd);
+        while( char_read != EOF ) {
+            *(file_contents+file_length) = char_read;
+            char_read = getc(fd);
+            file_length++;
+        }
+        return file_length;
+    } else {
+        // File did not exist, return a 404.
+        return 0;
+    }
+
+}
+
+int send_400_response(int connfd) {
+    if(send(connfd, "HTTP/1.1 400 BAD REQUEST\r\n\r\n400 BAD REQUEST", 27,0 ) == -1) {
+        // Error here too!
+        return -1;
+    }
+    return 0;
+}
 
 
 int send_packets(int connfd, char *file_contents, char* filepath) {
@@ -96,8 +102,6 @@ int send_packets(int connfd, char *file_contents, char* filepath) {
 
     if (bytes_to_send == 0) {
         send_file_not_found(connfd);
-    } else if (bytes_to_send == -1) {
-        send_server_error(connfd);
     }
 
     long to_send = 0;
@@ -107,7 +111,7 @@ int send_packets(int connfd, char *file_contents, char* filepath) {
         if (offset + PACKETSIZE > bytes_to_send) {
             to_send = bytes_to_send % PACKETSIZE;
         }
-        long amount_sent = send(connfd, file_contents+offset, to_send, 0);
+        long amount_sent = send(connfd, file_contents+offset, (size_t)to_send, 0);
         if (amount_sent == -1) {
             char *error_message = "Error sending to socket with file "\
                         "descriptor %d.\n";
@@ -123,7 +127,7 @@ int send_packets(int connfd, char *file_contents, char* filepath) {
 
 int check_request(char request[BUFFERLEN]) {
     if (request[0] == 'G' && request[1] == 'E' && request[2] == 'T') return 200;
-    return -1;
+    else return 400;
 }
 
 // Returns the length of the path in the request, puts the path in `char *filepath`.
@@ -138,6 +142,48 @@ int get_request_path(char request[BUFFERLEN], char *filepath) {
         *(filepath+index) = request[index+5];
     }
     return -1; //  WE MAXED OUT THE BUFFER AND SHOULD DEAL WITH THIS ERROR!
+}
+
+
+// Loop through the request until we get to a sequence that looks like "\nHost:".
+char *get_request_hostname(char request[BUFFERLEN]) {
+
+    // Initial setup.
+    char temp[6];
+    char *hostname = malloc(1024);
+    int index = 0;
+    int inner_index;
+    for (; index < 6; index++) {
+        temp[index] = request[index];
+    }
+
+    // Actual looping.
+    for (index = 6; index < BUFFERLEN; index++) {
+        for (inner_index = 0; inner_index < 5; inner_index++) {
+            temp[inner_index] = temp[inner_index + 1];
+        }
+        temp[5] = request[index];
+
+        // If we have the host, get the hostname and return it.
+        if (strcmp(&temp[0], "\nHost:") == 0) {
+
+            // Move index from the chars between the "host" and the hostname.
+            index++;
+            if (request[index] == ' ') index++;
+
+            // Loop to get the hostname, and return it.
+            for (inner_index = 0; inner_index < 1024; inner_index++){
+                // We're at the end of the hostname at ':', '\r' or '\n'.
+                if (request[index] == ':' || request[index] == '\r' || request[index] == '\n') {
+                    return hostname;
+                }
+                *(hostname + inner_index) = request[index];
+                index++;
+            }
+
+        }
+    } // Either we reached the end, or the hostname wasn't here.
+    return NULL;
 }
 
 int send_200_request(int connfd, char request[BUFFERLEN]) {
@@ -160,17 +206,25 @@ int send_200_request(int connfd, char request[BUFFERLEN]) {
             return -1;
         };
         
-        printf("Sent 200 request.\n");
-		
     }
     return 0;
 }
 
-int process_request(int connfd) {
+int process_request(int connfd, char *hostname) {
     if (!fork()) { // This is a child process
+
         char request[BUFFERLEN];
         memset(&request, ' ', BUFFERLEN);
         long recv_response = recv(connfd, request, BUFFERLEN, 0);
+
+        // If we have a bad hostname on this request, we can immediately exit.
+        char *request_hostname = get_request_hostname(request);
+        printf("%s\n%s\n", request_hostname, hostname);
+        if (strcmp(hostname, request_hostname) == 0) {
+            send_400_response(connfd);
+            return 0; // We sent a 400, but we dealt with it correctly, so no actual errors.
+        }
+
         if (recv_response == -1) {
             char *message = "Error reading! Errno: %d.\n";
             report_errno(message);
@@ -179,9 +233,7 @@ int process_request(int connfd) {
         if(check_request(request) == 200) {
             send_200_request(connfd, request);
         } else {
-            // We've got a request that isn't a GET...let's just throw a ...
-            //     ...500 error back, we have nothing to transfer.
-            send_server_error(connfd);
+            send_400_response(connfd);
         }
 
     }
@@ -192,13 +244,12 @@ int process_request(int connfd) {
     return 0;
 }
 
-int acceptConnections(int serverfd) {
+int acceptConnections(int serverfd, char *hostname) {
     // Now that we have a socket, let's accept a connection to it.
     int connfd;
     struct sockaddr_storage client_addr;
     socklen_t client_addr_len = sizeof client_addr;
     while(1) {
-        printf("Looping?\n");
         connfd = accept(serverfd, (struct sockaddr *)&client_addr, &client_addr_len);
         if (connfd == -1) {
             report_errno("Error accepting connection.");
@@ -207,34 +258,24 @@ int acceptConnections(int serverfd) {
             printf("Connection made!\n");
         
             // Process the request found at the connection, and return a server error if something goes wrong.
-            if (process_request(connfd) == -1) {
+            if (process_request(connfd, hostname) == -1) {
                 printf("Error processing request, sending server error message.\n");
                 send_server_error(connfd);
             }
         }
         close(connfd);
     }
-    // If we've git this, there's been an error accepting the connection.
+    // If we've got this, there's been an error accepting the connection.
     return -1;
 }
 
 
 int main(int argc, const char * argv[]) {
 
-    int index;
-    for(index = 0; index < argc; index++) {
-
-    }
-
-
-    // Before we do anything else, let's be aware of the path we're at.
-    printf("%s\n", argv[0]);
-    abs_path = (const char *)malloc(strlen(argv[0]) + 1);
-    abs_path = strcat((char *)argv[0], "/");
-    
     // Get address information.
     struct addrinfo hints, *server_info_results, *server_info;
     int serverfd = -1;
+    char *hostname = (char *)malloc(1024);
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
@@ -250,15 +291,13 @@ int main(int argc, const char * argv[]) {
     for(server_info = server_info_results; server_info != NULL; server_info = server_info->ai_next) {
         if ((serverfd = socket(server_info->ai_family, server_info->ai_socktype,
                                server_info->ai_protocol)) == -1) {
-            perror("server: socket");
             continue;
         }
-        
-        // reuse addresses
+
         int set_sock_opt = 1;
         if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &set_sock_opt, sizeof(int)) == -1) {
-            perror("setsockopt");
-            return 1;
+            // We hit an error setting options on this socket.
+            continue;
         }
         
         if (bind(serverfd, server_info->ai_addr, server_info->ai_addrlen) == -1) {
@@ -276,6 +315,16 @@ int main(int argc, const char * argv[]) {
         printf("We found no valid socket to bind to.\n");
         return 1; // Error.
     }
+    printf("We're on the other side...");
+
+    gethostname(hostname, MAXHOSTLENGTH);
+    struct hostent hostname_data = *gethostbyname(hostname);
+    hostname = hostname_data.h_name;
+printf("Hostname: ");
+    printf("%s\n", hostname);
+
+    // We've finished with out server_info struct, so let's free it!
+    freeaddrinfo(server_info);
     
     // Sockets are at their most useful when they are being listened to.
     int listen_result = listen(serverfd, BACKLOG);
@@ -286,15 +335,16 @@ int main(int argc, const char * argv[]) {
 
     
     // Loop for accepting.
-    while(1){
-        if (acceptConnections(serverfd) == -1) {
-            fprintf(stderr, "We got ourselves an error accepting connections! Aborting all plans.\n");
-            return 1; // Error, but we might not have a connfd, so let's not send a 500, eh?
-            break;
+    while(1) {
+        if (acceptConnections(serverfd, hostname) == -1) {
+            fprintf(stderr, "We got ourselves an error accepting "\
+                    "connections! Aborting all plans.\n");
+            break;  // Error, occurred, bomb out.
         }
     }
+
+    // Something went wrong. We'll kill the server here.
     close(serverfd);
-    
-    return 0;
-    
+    return 1;  //Error.
+
 }
