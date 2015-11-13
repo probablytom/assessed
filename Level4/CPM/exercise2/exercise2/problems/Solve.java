8192c4899e9cf903659268421eb8bc7d036c017f
Tom Wallis <tom@tom-wallis.com>
2015-11-13 17:11:12 +0000

    Began work on CP AE2

--- Level4/CPM/exercise2/exercise2/problems/Solve.java
+++ Level4/CPM/exercise2/exercise2/problems/Solve.java
@@ -12,12 +12,14 @@ import org.chocosolver.solver.constraints.*;
 public class Solve {
 	
 	Solver solver;
-	IntVar[] timeslot;
+	IntVar[] meetingTimes;
 	int nAgents;
 	int mMeetings;
 	int timeslots;
+	int[][] distanceMatrix;
+	int[][] attendanceMatrix;
 	
-	Solve(String filename) throws IOException {
+	public Solve(String filename) throws IOException {
 
 		solver = new Solver("solver");
 		
@@ -28,24 +30,91 @@ public class Solve {
 			nAgents = sc.nextInt();
 			timeslots = sc.nextInt();
 			
-			// Make the choco variables we'll be using
-			timeslot = VF.enumeratedArray("timeslots", timeslots, 0, mMeetings, solver);
+			// Make the variables we'll be using...
+			meetingTimes = VF.enumeratedArray("timeslots", mMeetings, 0, mMeetings - 1, solver); // Should this be -1?
+			attendanceMatrix = new int[nAgents][mMeetings];
+			distanceMatrix = new int[mMeetings][mMeetings];
 			
+			String temp;
 			
+			// Let's read in the data and populate our matrices. 
+			for (int agentIndex = 0; agentIndex < nAgents; agentIndex++) {
+				
+				// Skip the label at the beginning of the line
+				temp = sc.next();
+				
+				for (int meetingIndex = 0; meetingIndex < mMeetings; meetingIndex++) {
+					attendanceMatrix[agentIndex][meetingIndex] = sc.nextInt();
+				}
+			}
+			
+			for (int meeting1 = 0; meeting1 < mMeetings; meeting1++){
+				
+				// Skip the label at the beginning of the line
+				temp = sc.next();
+				
+				for (int meeting2 = 0; meeting2 < mMeetings; meeting2++) {
+					distanceMatrix[meeting1][meeting2] = sc.nextInt();
+				}
+			}
+			
+			// We've read in all of the data, so now, we can make our Choco constraints. 
+			// If we find an agent who's going to two meetings, those two meetings must have timeslots of their distance + 1 or greater.
+			
+			for (int agentIndex = 0; agentIndex < nAgents; agentIndex++){
+				for (int meeting1 = 0; meeting1 < mMeetings-1; meeting1++){
+					for (int meeting2 = meeting1 + 1; meeting2 < mMeetings; meeting2++){
+						
+						// Create the constraint if we're attending both meetings
+						if (attendanceMatrix[agentIndex][meeting1] == 1 && 
+							attendanceMatrix[agentIndex][meeting2] == 1) {
+							System.out.println("Constraint posted!");
+							solver.post(ICF.distance(meetingTimes[meeting1], meetingTimes[meeting2], ">", distanceMatrix[meeting1][meeting2]));
+						
+						}
+						
+					}
+				}
+			}
 			
 		}
 		
 	}
+	
+	public boolean findSolution() {
+		return solver.findSolution();
+	}
+	
+	public void printSolution() {
+		for (int i = 0; i < mMeetings; i++) {
+			System.out.println(Integer.toString(i) + ": " + meetingTimes[i].getValue());
+		}
+	}
 
 	public static void main(String[] args) {
 		
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+		
 		
 		if (args.length == 1) {
-			// No arguments, nothing to process
+			try {
+				Solve solution = new Solve(args[0]);
+				boolean resultFound = solution.findSolution();
+				if (resultFound) {
+					
+					solution.printSolution();
+					
+				}
+				// process output of solver
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else if (args.length == 2) {
-			// We're passed a file to process, but no time to return in.
+			
 		} else {
-			// Some edge case we should take care of
+			// The same, but with a timer
 		}
 
 	}
