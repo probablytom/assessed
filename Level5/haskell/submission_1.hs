37e0bb9a4966b5d68de734e0094e60feceee411e
Tom Wallis <github@tomwallis.net>
2016-10-20 23:41:28 +0000

    Implemented with beautiful applicatives

--- Level5/haskell/submission_1.hs
+++ Level5/haskell/submission_1.hs
@@ -1,6 +1,6 @@
 data Tree a = Leaf | Node a (Tree a) (Tree a)  deriving (Show, Ord, Eq, Read)
 
--- Gosh! Exotic! We make `Tree` a Functor, so we can recursively map along it.
+-- A functor allows you to map a function across values trapped in a datatype (Tree) using fmap
 instance Functor Tree where
   fmap f Leaf = Leaf
   fmap f (Node elem left right) = Node newelem newleft newright
@@ -8,16 +8,17 @@ instance Functor Tree where
           newleft  = fmap f left
           newright = fmap f right
 
-treezip :: (Tree a) -> (Tree b) -> (Tree (a,b))
-treezip (Node e1 l1 r1) (Node e2 l2 r2) = Node new_e new_l new_r
-  where new_e = (e1, e2)
-        new_l = treezip l1 l2
-        new_r = treezip r1 r2
-treezip _ _ = Leaf  -- Didn't match the above pattern so *must* be Leaf
+-- An Applicative types can apply against each other. <*> :: f (a->b) -> f a -> f b (and it's infix)
+-- Note: for an Applicative, you can use <$> as an infix version of fmap!
+instance Applicative Tree where
+  pure a = Node a Leaf Leaf
+  Node e1 l1 r1 <*> Node e2 l2 r2 = Node (e1 e2) (l1 <*> l2) (r1 <*> r2)
+  _ <*> _ = Leaf
 
--- Exploit Tree being a Functor to fmap a lambda through it
+-- We make use of Tree being an Applicative
+treezip :: (Tree a) -> (Tree b) -> (Tree (a,b))
+treezip first second = (,) <$> first <*> second
+
+-- We make use of Tree being a Functor
 treeunzip :: (Tree (a,b)) -> (Tree a, Tree b)
-treeunzip Leaf = (Leaf, Leaf)
-treeunzip intree = (fmap get_left intree, fmap get_right intree)
-  where get_left  = \(x, y) -> x
-        get_right = \(x, y) -> y
+treeunzip intree = (fmap fst intree, fmap snd intree)
