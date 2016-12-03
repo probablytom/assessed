ad3958eb1e6c6e15fc7c7daae89b94e72eed0055
Tom Wallis <github@tomwallis.net>
2016-12-03 22:25:06 +0000

    nearly working AE2

---
+++ Level5/haskell/hatest.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Text.LaTeX
+
+-- By executing 'execLaTeXT' you run the 'LaTeXT' monad and make a 'LaTeX' value as output.
+-- With 'renderFile' you render it to 'Text' and write it in a file.
+main :: IO ()
+main = execLaTeXT simple >>= renderFile "simple.tex"
+
+-- It's a good idea to separate the preamble of the body.
+simple :: Monad m => LaTeXT_ m
+simple = do
+ thePreamble
+ document theBody
+
+-- Preamble with some basic info.
+thePreamble :: Monad m => LaTeXT_ m
+thePreamble = do
+ documentclass [] article
+ author "Daniel Diaz"
+ title "Simple example"
+
+-- Body with a section.
+theBody :: Monad m => LaTeXT_ m
+theBody = do
+ maketitle
+ section "Hello"
+ "This is a simple example using the "
+ hatex
+ " library. "
+ -- 'textbf' turns characters to bold font (as you already may know).
+ textbf "Enjoy!"
+ " "
+ -- This is how we nest commands.
+ textbf (large "Yoohoo!")
+
--- Level5/haskell/submission_2.hs
+++ Level5/haskell/submission_2.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Text.XML.HXT.Core
 import Text.HandsomeSoup
 import Data.List.Split
 import Data.Maybe
-import Control.Monad.IO.Class
+--import Control.Monad.IO.Class
+import Data.Char (isDigit)
+import Text.LaTeX
+import Data.Text (pack)
 
-type PhonebookEntry = (String, String)
-
-type PhoneBook = [PhonebookEntry]
+data PhonebookEntry = Entry (String, String) deriving Show
 
 rootUrl = "http://www.gla.ac.uk/schools/computing/staff/"
 
@@ -14,28 +17,61 @@ rootUrl = "http://www.gla.ac.uk/schools/computing/staff/"
 staffSection :: String -> String
 staffSection str = (last $ splitOn "/" str) ++ "/"
 
-parseStaffEntry :: String -> IO PhonebookEntry
+parseStaffEntry :: String -> IO (Maybe PhonebookEntry)
 parseStaffEntry staffurl = do
   let source = fromUrl staffurl
-  let name = runX $ source >>> css "h1.responsivestyle" /> getText
-  let phoneNumber = runX $ source >>> css "div#sp_contactInfo" /> getText
-  contructEntry name phoneNumber
+  name <- runX $ source >>> css "h1.responsivestyle" /> getText
+  phoneNumber <- runX $ source >>> css "div#sp_contactInfo" //> getText
+  return $ contructEntry name phoneNumber
 
-contructEntry :: IO [String] -> IO [String] -> IO PhonebookEntry
-contructEntry io_name io_phoneNumber = do
-  name <- io_name
-  phoneNumber <- io_phoneNumber
-  return (name !! 0, phoneNumber !! 0)
+contructEntry :: [String] -> [String] -> Maybe PhonebookEntry
+contructEntry name phoneNumber = if validName name && validNumber phoneNumber
+  then Just $ Entry (name !! 0, findNumber phoneNumber)
+  else Nothing
+   where
+    validName name = length name /= 0
+    validNumber phoneNumber = "telephone" `elem` phoneNumber
 
--- TODO: To parse out the phone number from the contents of the contact info block.
-findNumber :: String -> String
-findNumber x = x
+-- TODO: Don't just select the second element, select the element after "telephone"
+findNumber :: [String] -> String
+findNumber x = [n | n <- (x !! 2), isDigit n]
 
-main :: IO PhoneBook
+phoneDirectory :: Monad m => [PhonebookEntry] -> LaTeXT_ m
+phoneDirectory directory = do
+  preamble
+  document $ createDirectory directory
+
+-- A small preamble creating section
+preamble :: Monad m => LaTeXT_ m
+preamble = do
+  documentclass [] article
+  author "Tom Wallis"
+  title "Functional Programming A.E. 2"
+
+-- Rendering and layout of a [PhonebookEntry] directory to LaTeX
+createDirectory :: Monad m => [PhonebookEntry] -> LaTeXT_ m
+createDirectory directory = do
+  maketitle
+  section "Directory"
+  center $ tabular Nothing [CenterColumn, LeftColumn] $ do
+    let entries = fmap (fmap pack) [[name, number] | (Entry (name, number)) <- directory]
+    foldr render_entry (return ()) entries
+     where
+      render_entry :: (Monad m, Texy t) => [t] -> LaTeXT_ m -> LaTeXT_ m
+      render_entry [name, number] l = do
+        texy name & texy number
+        hline
+        l
+       
+
+-- To be run
+main :: IO ()
 main = do
   let rootSource = fromUrl rootUrl
   staffSegments <- runX $ rootSource >>> css "ul#research-teachinglist li a" ! "href"
   let staffUrls = fmap (\x -> rootUrl ++ (staffSection x)) staffSegments
-  let phonebook_entries = foldr (\url book -> book ++ [parseStaffEntry url]) [return (("","")::PhonebookEntry)] staffUrls
-  sequence phonebook_entries
-
+  let phonebook_entries = foldr (\url book -> book ++ [parseStaffEntry url]) [] (take 3 staffUrls)
+  unfiltered_phonebook <- sequence phonebook_entries
+  let remove_empty_entries = (map fromJust) . (filter isJust)
+  let phonebook = remove_empty_entries unfiltered_phonebook
+  execLaTeXT (phoneDirectory phonebook) >>= renderFile "directory.tex"
