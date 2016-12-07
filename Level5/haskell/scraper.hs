d8cf1daa4a7a05f5e21025960e756578209c67ee
Tom Wallis <github@tomwallis.net>
2016-12-07 10:55:46 +0000

    Fully commented, checks and balances

--- Level5/haskell/scraper.hs
+++ Level5/haskell/scraper.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- NOTE to run, you'll need to have HaTeX and HandsomeSoup installed: --
+-- cabal install HaTeX
+-- cabal install HandsomeSoup
+-- cabal configure
+
 import Text.XML.HXT.Core
 import Text.HandsomeSoup
-import Data.List.Split (splitOn)
-import Data.Maybe
-import Data.Char (isDigit)
 import Text.LaTeX
 import Data.Text (pack)
+import Data.List.Split (splitOn)
+import Data.Maybe (fromJust, isJust)
+import Data.Char (isDigit)
+
 -- Longtable definition from Jeremy Singer. Thanks, Jeremy!
 import Text.LaTeX.Base.Class
 import Text.LaTeX.Base.Commands
@@ -18,11 +24,15 @@ data PhonebookEntry = Entry (String, String) deriving Show
 
 rootUrl = "http://www.gla.ac.uk"
 
+-----
+-- ** Here we process URLs and produce staff entries. ** --
+-----
 
 -- Convenience function to get the last part of a string separated by '/'
 staffSection :: String -> String
 staffSection str = (last $ splitOn "/" str) ++ "/"
 
+-- Take a URL and extract a phonebook entry if it exists
 parseStaffEntry :: String -> IO (Maybe PhonebookEntry)
 parseStaffEntry staffurl = do
   let source = fromUrl staffurl
@@ -30,6 +40,7 @@ parseStaffEntry staffurl = do
   phoneNumber <- runX $ source >>> css "div#sp_contactInfo" //> getText
   return $ contructEntry name phoneNumber
 
+-- Once the data has been extracted from the URL, process it and construct the PhonebookEntry (if it exists)
 contructEntry :: [String] -> [String] -> Maybe PhonebookEntry
 contructEntry name phoneNumber = if validName name && validNumber phoneNumber
   then Just $ Entry (name !! 0, findNumber phoneNumber)
@@ -38,10 +49,16 @@ contructEntry name phoneNumber = if validName name && validNumber phoneNumber
     validName name = length name /= 0
     validNumber phoneNumber = "telephone" `elem` phoneNumber
 
--- TODO: Don't just select the second element, select the element after "telephone"
+-- Extracting the number from its enclosing div
+-- (It can't be specified by CSS2 selector, so can't use HandsomeSoup)
 findNumber :: [String] -> String
 findNumber x = [n | n <- (x !! 2), or [isDigit n, n == '+']]
 
+-----
+-- ** Here we write the LaTeX document that acts as output. ** --
+-----
+
+-- Write the LaTeX document for the phone directory
 phoneDirectory :: Monad m => [PhonebookEntry] -> LaTeXT_ m
 phoneDirectory directory = do
   preamble
@@ -71,7 +88,7 @@ createDirectory directory = do
         hline
         l
 
--- To be run
+-- The main loop of the code. Will kick the rest of the processing off. 
 main :: IO ()
 main = do
   let rootSource = fromUrl $ rootUrl ++ "/schools/computing/staff/"
@@ -79,6 +96,7 @@ main = do
   let staffUrls = fmap (\x -> rootUrl ++ x ++ "/") staffPartialUrls
   let phonebook_entries = foldr (\url book -> book ++ [parseStaffEntry url]) [] staffUrls
   unfiltered_phonebook <- sequence phonebook_entries
-  let remove_empty_entries = (map fromJust) . (filter isJust)
   let phonebook = remove_empty_entries unfiltered_phonebook
   execLaTeXT (phoneDirectory phonebook) >>= renderFile "directory.tex"
+   where
+    remove_empty_entries = (map fromJust) . (filter isJust)
